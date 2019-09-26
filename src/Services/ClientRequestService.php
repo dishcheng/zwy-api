6bc9d716eb89e4e6571df4fb8bae2b3e8b628619
caicheng <caicheng@hooook.com>
2019-09-26 21:26:49 +0800

    自我游封装处理

--- src/Services/ClientRequestService.php
+++ src/Services/ClientRequestService.php
@@ -5,6 +5,7 @@ namespace DishCheng\ZwyApi\Services;
 use DishCheng\ZwyApi\Exceptions\ZwyApiException;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 /**
  * 服务端请求自我游服务端
@@ -155,16 +156,32 @@ class ClientRequestService
                     'data' => $resArr,
                 ];
             } else {
+                //请求出错
                 $msg = $err_header . 'STATUS WRONG';
                 Log::error($msg, [
                     'path' => $path,
                     'data' => $data,
                     'res' => $resArr,
                 ]);
-                return [
-                    'status' => false,
-                    'msg' => $msg . ':' . $resArr['msg'] ?? '',
-                ];
+                if (Str::contains($resArr['msg'], '程序错误')) {
+                    /**
+                     * 这样做的原因是自我游那边返回的错误在包含程序错误的时候会把请求出去的参数带上去，
+                     * 我又是直接抛回前端有点蛋疼，下面拿的错误有时候会在下单出现（极少数情况下）
+                     *
+                     * 程序错误，接收数据为：<?xml version="1.0" encoding="utf-8"?><root><travel_date>2019-09-27</travel_date><info_id>25525032</info_id><cust_id>2976618</cust_id><num>1</num><link_man>cai</link_man><link_phone>13627227858</link_phone></root>↵
+                     * 抛出异常为：TransactionException in DbTransactionManager.java:commit()
+                     */
+                    return [
+                        'status' => false,
+                        'msg' => $msg . ':zwy程序错误' ?? '',
+                    ];
+                } else {
+                    return [
+                        'status' => false,
+                        'msg' => $msg . ':' . $resArr['msg'] ?? '',
+                    ];
+                }
+
             }
         } else {
             //返回的不是xml，出错
